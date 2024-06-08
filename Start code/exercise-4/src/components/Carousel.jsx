5ece848a0886678cc9a34d4c42fad8eca6f92051
Hoàng <hoanghung20030@gmail.com>
2024-06-08 10:52:24 +0700

    Finish exercise 4

--- Start code/exercise-4/src/components/Carousel.jsx
+++ Start code/exercise-4/src/components/Carousel.jsx
@@ -1,21 +1,33 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
 
 export const Carousel = ({ images }) => {
   /* You will need to  use  state to mnage the current image */
-
+  const [currentImage, setCurrentImage] = useState(0);
+useEffect(()=>{
+  console.log(currentImage);
+},[currentImage])
   /* You will need to hanle the click on left and right button */
+  const handleLeft = () => {
+    setCurrentImage((prev) => (prev === 0 ? images.length - 1 : prev - 1));
+   
+  };
 
-  /* You will need to manage the cases when we are on the last image or first image*/
+  const handleRight = () => {
+    setCurrentImage((prev) => (prev === images.length - 1 ? 0 : prev + 1));
+  };
 
   return (
     <div className="carousel">
-      <BsArrowLeftCircleFill className="arrow arrow-left" />
+      <BsArrowLeftCircleFill className="arrow arrow-left" onClick={handleLeft} />
 
       {/* YOu will need to display the current image, not the first one.. */}
-      <img src={images[0].src} alt={images[0].alt} className="slide" />
+      <img src={images[currentImage].src} alt={images[currentImage].alt} className="slide" />
 
-      <BsArrowRightCircleFill className="arrow arrow-right" />
+      <BsArrowRightCircleFill className="arrow arrow-right" onClick={handleRight} />
     </div>
   );
 };
+
