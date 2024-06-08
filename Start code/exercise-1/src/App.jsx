e9cd4cc62ce16bd7eb2899f17de50cee1024e54a
Hoàng <hoanghung20030@gmail.com>
2024-06-08 10:06:35 +0700

    Finish exercise 1

--- Start code/exercise-1/src/App.jsx
+++ Start code/exercise-1/src/App.jsx
@@ -1,28 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
 function App() {
-  /* You will need to use a boolean state to manage the weather */
+  // Manage the weather state
+  const [isRaining, setIsRaining] = useState(false);
+  const [title, setTitle] = useState("Sunny time");
+  const [background, setBackground] = useState("sunny");
+
+  useEffect(() => {
+    // Update background color when isRaining changes
+    getBackgroundColor();
+  }, [isRaining]);
 
   function onSunClick() {
-    // Complete this code when we click on Sunny Time
+      // Complete this code when we click on Sunny Time
+    setIsRaining(false);
+    getTitle(false);
   }
 
   function onRainClick() {
-    // Complete this code when we click on Sunny Time
+    // Set weather to rainy and update title
+    setIsRaining(true);
+    getTitle(true);
   }
 
-  function getTitle() {
-    // This function manage the H1 text, depending on the weather
+  function getTitle(isRaining) {
+    // Update title based on the weather
+    if (isRaining) {
+      setTitle("Rain Time");
+    } else {
+      setTitle("Sunny Time");
+    }
   }
 
   function getBackgroundColor() {
-    // This function manage the main class value, depending on the weather
+     // This function manage the main class value, depending on the weather
+    if (isRaining) {
+      setBackground("rainy");
+    } else {
+      setBackground("sunny");
+    }
   }
 
   return (
-    <main>
-      <h1>TODO</h1>
-      <button>Sunny Time</button>
-      <button>Rain Time</button>
+    <main className={background}>
+      <h1>{title}</h1>
+      <button onClick={onSunClick}>Sunny Time</button>
+      <button onClick={onRainClick}>Rain Time</button>
     </main>
   );
 }
